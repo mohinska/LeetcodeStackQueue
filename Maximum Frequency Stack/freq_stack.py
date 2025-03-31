97a9b1903ce3d72cb67c36f6fe80ebe5a6f17b79
Maryna Ohinska <maryna.ohinska@gmail.com>
2025-03-31 17:49:33 +0300

    Using defaultdict in frequency stack

--- Maximum Frequency Stack/freq_stack.py
+++ Maximum Frequency Stack/freq_stack.py
@@ -1,49 +1,16 @@
-from collections import deque
-import unittest
+from collections import defaultdict
 
 class FreqStack:
-
     def __init__(self):
-        self.stack = deque()
-        self.freq_table = dict()
-
+        self.freq_table = defaultdict(int)
+        self.freq_group = defaultdict(list)
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        self.freq_table[val] = self.freq_table.get(val, 0) + 1
-
+        self.freq_table[val] += 1
+        self.freq_group[self.freq_table[val]].append(val)
 
     def pop(self) -> int:
-        i = -1
-        while max(self.freq_table.values()) != self.freq_table[self.stack[i]]:
-            i -= 1
-        self.freq_table[self.stack[i]] -= 1
-        el = self.stack[i]
-        del self.stack[i]
-        return el
-
-
-class TestFreqStack(unittest.TestCase):
-    def test_freq_stack(self):
-        # Create a FreqStack instance
-        fs = FreqStack()
-
-        # Push elements onto the stack
-        fs.push(5)
-        fs.push(7)
-        fs.push(5)
-        fs.push(7)
-        fs.push(4)
-        fs.push(5)
-        # The stack is [5, 7, 5, 7, 4, 5]
-
-        # Expected pop sequence
-        self.assertEqual(fs.pop(), 5)  # 5 is the most frequent
-        # 7 and 5 had the same frequency, but 7 is closer to the top
-        self.assertEqual(fs.pop(), 7)
-        self.assertEqual(fs.pop(), 5)  # 5 is still the most frequent
-        self.assertEqual(fs.pop(), 4)  # 4 is the remaining element
-
-
-if __name__ == "__main__":
-    unittest.main()
+        max_freq = max(self.freq_table.values())
+        val = self.freq_group[max_freq].pop()
+        self.freq_table[val] -= 1
+        return val
