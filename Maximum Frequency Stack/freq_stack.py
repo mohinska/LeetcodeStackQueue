a443878b34dbe386fbefea78ae686488e0d20c96
Maryna Ohinska <maryna.ohinska@gmail.com>
2025-03-31 17:00:48 +0300

    Using deque in frequency stack

--- Maximum Frequency Stack/freq_stack.py
+++ Maximum Frequency Stack/freq_stack.py
@@ -1,8 +1,10 @@
+from collections import deque
 import unittest
+
 class FreqStack:
 
     def __init__(self):
-        self.stack = []
+        self.stack = deque()
         self.freq_table = dict()
 
 
@@ -16,30 +18,32 @@ class FreqStack:
         while max(self.freq_table.values()) != self.freq_table[self.stack[i]]:
             i -= 1
         self.freq_table[self.stack[i]] -= 1
-        return self.stack.pop(i)
+        el = self.stack[i]
+        del self.stack[i]
+        return el
 
 
-# class TestFreqStack(unittest.TestCase):
-#     def test_freq_stack(self):
-#         # Create a FreqStack instance
-#         fs = FreqStack()
+class TestFreqStack(unittest.TestCase):
+    def test_freq_stack(self):
+        # Create a FreqStack instance
+        fs = FreqStack()
 
-#         # Push elements onto the stack
-#         fs.push(5)
-#         fs.push(7)
-#         fs.push(5)
-#         fs.push(7)
-#         fs.push(4)
-#         fs.push(5)
-#         # The stack is [5, 7, 5, 7, 4, 5]
+        # Push elements onto the stack
+        fs.push(5)
+        fs.push(7)
+        fs.push(5)
+        fs.push(7)
+        fs.push(4)
+        fs.push(5)
+        # The stack is [5, 7, 5, 7, 4, 5]
 
-#         # Expected pop sequence
-#         self.assertEqual(fs.pop(), 5)  # 5 is the most frequent
-#         # 7 and 5 had the same frequency, but 7 is closer to the top
-#         self.assertEqual(fs.pop(), 7)
-#         self.assertEqual(fs.pop(), 5)  # 5 is still the most frequent
-#         self.assertEqual(fs.pop(), 4)  # 4 is the remaining element
+        # Expected pop sequence
+        self.assertEqual(fs.pop(), 5)  # 5 is the most frequent
+        # 7 and 5 had the same frequency, but 7 is closer to the top
+        self.assertEqual(fs.pop(), 7)
+        self.assertEqual(fs.pop(), 5)  # 5 is still the most frequent
+        self.assertEqual(fs.pop(), 4)  # 4 is the remaining element
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
